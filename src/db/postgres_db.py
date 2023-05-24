d2ac20fb790b0a2069638bf3e661c306764c05e4
tyshbyr <tyshbyr@gmail.com>
2023-05-25 02:33:41 +0500

    Fixed, tables created

--- src/db/postgres_db.py
+++ src/db/postgres_db.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
+from src.models import *
 
 
 def init_db(app: Flask):
-    import src.models
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://auth_user:123qwe@localhost/auth_db'
     db.init_app(app)
