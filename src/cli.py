2a0adf433d2647d46035c96440ded931f4ae70fc
tyshbyr <tyshbyr@gmail.com>
2023-06-03 17:52:42 +0500

    Add click echo

--- src/cli.py
+++ src/cli.py
@@ -15,10 +15,12 @@ createsuperuser_bp = Blueprint("create", __name__)
 def create_superuser(username, password):
     user = db.session.query(User).filter_by(username=username).first()
     if user:
-        print("Пользователь с таким username уже существует")
+        click.echo(
+            click.style("Пользователь с таким username уже существует", fg="yellow")
+        )
         return
 
     user = User(username=username, password=password, is_superuser=True)
     db.session.add(user)
     db.session.commit()
-    print("Суперпользователь успешно создан.")
+    click.echo(click.style("Суперпользователь успешно создан.", fg="green"))
