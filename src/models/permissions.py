38f053f08230d01ffb05e889ef933887e0613eab
Slender357 <114290259+Slender357@users.noreply.github.com>
2023-06-03 15:41:46 +0300

    fix for isues

--- src/models/permissions.py
+++ src/models/permissions.py
@@ -4,15 +4,6 @@ from src.db.postgres_db import db
 from src.models.mixin import Mixin
 
 
-# RolesPermissions = db.Table('roles_permissions',
-#                             db.Column('user_id', UUID(as_uuid=True),
-#                                       db.ForeignKey('roles.id',
-#                                                     ondelete='CASCADE'),
-#                                       primary_key=False),
-#                             db.Column('role_id', UUID(as_uuid=True),
-#                                       db.ForeignKey('permissions.id',
-#                                                     ondelete='CASCADE'),
-#                                       primary_key=False))
 class RolesPermissions(db.Model):
     __tablename__ = "roles_permissions"
 
@@ -32,35 +23,3 @@ class Permission(Mixin):
 
     def __repr__(self):
         return f"<Permission {self.permission}>"
-
-
-
-# ```
-#
-#
-# class Permission(Base):
-#     __tablename__ = 'permissions'
-#
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String, nullable=False)
-#
-#     roles = relationship('Role', secondary='roles_permissions',
-#                          overlaps="permissions,roles_relation")
-#
-#
-# class Role(Base):
-#     __tablename__ = 'roles'
-#
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String, nullable=False)
-#
-#     permissions = relationship('Permission', secondary='roles_permissions')
-#
-#
-# class RolesPermissions(Base):
-#     __tablename__ = 'roles_permissions'
-#
-#     permission_id = Column(Integer, ForeignKey('permissions.id'),
-#                            primary_key=True)
-#     role_id = Column(Integer, ForeignKey('roles.id'), primary_key=True)
-#     grant = Column(Boolean, nullable=False)
