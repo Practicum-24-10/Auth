8bf5d270e537b46d0e59bd30d54827689605a5ab
tyshbyr <tyshbyr@gmail.com>
2023-06-10 01:47:07 +0500

    Add jaeger_trace decorator

--- src/jaeger_tracer.py
+++ src/jaeger_tracer.py
@@ -1,3 +1,6 @@
+from functools import wraps
+
+from flask import request
 from opentelemetry import trace
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.sdk.trace import TracerProvider
@@ -22,3 +25,25 @@ def configure_tracer() -> None:
     trace.get_tracer_provider().add_span_processor(
         BatchSpanProcessor(ConsoleSpanExporter())
     )
+
+
+tracer = trace.get_tracer(__name__)
+
+
+if DEBUG:
+    def jaeger_trace(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            return f(*args, **kwargs)
+        return wrapper
+else:
+    def jaeger_trace(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            name = f.__name__
+            with tracer.start_as_current_span(name) as span:
+                request_id = request.headers.get("X-Request-Id")
+                span.set_attribute("http.request_id", request_id)
+                return f(*args, **kwargs)
+
+        return wrapper
